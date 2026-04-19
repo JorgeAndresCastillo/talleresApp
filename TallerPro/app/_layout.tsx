6d78763f566037c7338ba50f91b62d2274572563
SnowCast <snowcast@kali>
2026-04-19 16:32:06 +0200

    fix: simplify layout without auth check on startup

--- TallerPro/app/_layout.tsx
+++ TallerPro/app/_layout.tsx
@@ -4,9 +4,9 @@ import { StatusBar } from 'expo-status-bar';
 export default function RootLayout() {
   return (
     <>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="login" options={{ headerShown: false }} />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="login" />
+        <Stack.Screen name="index" />
       </Stack>
       <StatusBar style="auto" />
     </>
